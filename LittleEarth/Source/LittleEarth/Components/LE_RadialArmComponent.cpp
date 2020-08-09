0790c48574edaa3e6abd8bb199936808fc22290d
Artur Khayrullin <kehel@yandex.ru>
2020-08-09 14:49:32 +0500

    Radial arm component p1

---
+++ LittleEarth/Source/LittleEarth/Components/LE_RadialArmComponent.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LE_RadialArmComponent.h"
+
+const FName ULE_RadialArmComponent::SocketName(TEXT("RadialEndpoint"));
+
+ULE_RadialArmComponent::ULE_RadialArmComponent() {
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.TickGroup = TG_PostPhysics;
+
+	bAutoActivate = true;
+	bTickInEditor = true;
+
+	TargetArmLength = 300.0f;
+	CameraLagSpeed = 10.f;
+}
+
+void ULE_RadialArmComponent::OnRegister() {
+	Super::OnRegister();
+	UpdateSocketTransform(false, 0.f);
+}
+
+void ULE_RadialArmComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UpdateSocketTransform(bEnableCameraLag, DeltaTime);
+}
+
+void ULE_RadialArmComponent::UpdateSocketTransform(bool enableCameraLag, float DeltaTime) {
+	const FVector PreviousSocketLocation = SocketLocation;
+
+	FVector DesiredSocketLoc;
+
+	if (enableCameraLag) {
+		SocketLocation = FMath::VInterpTo(PreviousSocketLocation, DesiredSocketLoc, DeltaTime, CameraLagSpeed);
+	}
+	else {
+		SocketLocation = DesiredSocketLoc;
+	}
+}
+
+FTransform ULE_RadialArmComponent::GetSocketTransform(FName InSocketName, ERelativeTransformSpace TransformSpace) const {
+	FTransform RelativeTransform(SocketRotation, SocketLocation);
+
+	switch (TransformSpace) {
+	case RTS_World:
+	{
+		return RelativeTransform * GetComponentTransform();
+		break;
+	}
+	case RTS_Actor:
+	{
+		if (const AActor* Actor = GetOwner()) {
+			FTransform SocketTransform = RelativeTransform * GetComponentTransform();
+			return SocketTransform.GetRelativeTransform(Actor->GetTransform());
+		}
+		break;
+	}
+	case RTS_Component:
+	{
+		return RelativeTransform;
+	}
+	}
+	return RelativeTransform;
+}
+
+bool ULE_RadialArmComponent::HasAnySockets() const {
+	return true;
+}
+
+void ULE_RadialArmComponent::QuerySupportedSockets(TArray<FComponentSocketDescription>& OutSockets) const {
+	new (OutSockets) FComponentSocketDescription(SocketName, EComponentSocketType::Socket);
+}
+
---
+++ LittleEarth/Source/LittleEarth/Components/LE_RadialArmComponent.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "LE_RadialArmComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LITTLEEARTH_API ULE_RadialArmComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	ULE_RadialArmComponent();
+
+	virtual void OnRegister() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	static const FName SocketName;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Radial Arm")
+		AActor* Center = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Radial Arm")
+		FRotator OffsetRotator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Radial Arm")
+		float TargetArmLength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Radial Arm")
+		uint32 bEnableCameraLag : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Radial Arm", meta = (editcondition = "bEnableCameraLag", ClampMin = "0.0", ClampMax = "1000.0", UIMin = "0.0", UIMax = "1000.0"))
+		float CameraLagSpeed;
+
+	FVector SocketLocation;
+	FQuat SocketRotation;
+
+	// USceneComponent interface
+	virtual bool HasAnySockets() const override;
+	virtual FTransform GetSocketTransform(FName InSocketName, ERelativeTransformSpace TransformSpace = RTS_World) const override;
+	virtual void QuerySupportedSockets(TArray<FComponentSocketDescription>& OutSockets) const override;
+	// End of USceneComponent interface
+
+	void UpdateSocketTransform(bool enableCameraLag, float DeltaTime);
+};
