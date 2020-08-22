6c7d0f3376a6f879e85d0b1582c9f803ab661db6
Artur Khayrullin <kehel@yandex.ru>
2020-08-22 16:26:36 +0500

    Test Engine Adv Vehicle class with the same task

---
+++ LittleEarth/Source/LittleEarth/Test/AdvVehicleWheelFront.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleWheel.h"
+#include "AdvVehicleWheelFront.generated.h"
+
+UCLASS()
+class UAdvVehicleWheelFront : public UVehicleWheel
+{
+	GENERATED_BODY()
+
+public:
+	UAdvVehicleWheelFront();
+};
+
+
+
---
+++ LittleEarth/Source/LittleEarth/Test/AdvVehicleWheelRear.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleWheel.h"
+#include "AdvVehicleWheelRear.generated.h"
+
+UCLASS()
+class UAdvVehicleWheelRear : public UVehicleWheel
+{
+	GENERATED_BODY()
+
+public:
+	UAdvVehicleWheelRear();
+};
+
+
+
